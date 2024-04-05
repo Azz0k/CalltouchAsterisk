ba5efd66c0f7805ce0e26fd186ca142bfc54e723
Павел Милокум <milokum.pavel@dcenergoarm.local>
2024-04-05 16:15:30 +0400

    first commit

---
+++ Calltouch.py
@@ -0,0 +1,52 @@
+from imports.asterisk_db import Asterisk
+from imports.config import *
+import requests
+import logging
+import asyncio
+
+db = Asterisk()
+
+
+def send_calls():
+    db.connect()
+    calls = db.get_calls()
+    if len(calls) == 0:
+        logging.log(logging.INFO, 'Nothing to send.')
+    for call in calls:
+        unique_id = call[0]
+        call_src = call[1]
+        call_dst = '374' + call[3]
+        call_date = call[4].strftime('%Y-%m-%d %H:%M:%S')
+        payload = DEFAULT_PAYLOAD.copy()
+        payload['callid'] = unique_id
+        payload['callerid'] = call_src
+        payload['phonenumber'] = call_dst
+        payload['destphonenumber'] = call_dst
+        payload['date'] = call_date
+        payload['duration'] = call[5]
+        try:
+            logging.log(logging.INFO, payload)
+            result = requests.get(ATS_URL,
+                                  params={'src': call_src, 'dst': call_dst, 'date': call_date, 'id': unique_id})
+        except requests.exceptions.RequestException as e:
+            logging.log(logging.ERROR, e)
+        else:
+            if result.status_code == 200:
+                db.update_calls(unique_id, call_date, result.text)
+                logging.log(logging.INFO, 'Sent successfully, log id: ' + result.text)
+            else:
+                logging.log(logging.ERROR, result.text)
+    db.close()
+
+
+async def worker():
+    while True:
+        send_calls()
+        await asyncio.sleep(WORKER_DELAY_IN_SECONDS)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s %(name)s %(levelname)s %(message)s',
+                        datefmt='%H:%M:%S', filename=LOG_FILE_NAME)
+    asyncio.run(worker())
---
+++ imports/asterisk_db.py
@@ -0,0 +1,63 @@
+import mysql.connector
+from datetime import datetime
+from imports.config import *
+import logging
+from mysql.connector import errorcode
+
+
+class Asterisk:
+    def __init__(self):
+        self.host = HOST
+        self.table = TABLE
+        self.database = DATABASE
+        self.login = LOGIN
+        self.password = PASSWORD
+        self.db = None
+        self.connected = False
+
+    def get_calls(self, date='undefined'):
+        if not self.connected:
+            return []
+        if date == 'undefined':
+            date = datetime.now().strftime('%Y-%m-%d') + '%'
+        if not date.endswith('%'):
+            date = date + '%'
+        cursor = self.db.cursor()
+        query = 'SELECT uniqueid, src,dst,realdst,calldate,duration,userfield FROM cdr WHERE calldate like %s  and ' \
+                'disposition="ANSWERED" and realdst like "12%" and ' \
+                'src like "3%" and realdst!="12910303" and userfield="";'
+        cursor.execute(query, (date,))
+        result = cursor.fetchall()
+        cursor.close()
+        return result
+
+    def update_calls(self, unique_id, date, log_id):
+        if not self.connected:
+            return
+        cursor = self.db.cursor()
+        query = 'UPDATE cdr SET userfield=%s WHERE calldate like %s and uniqueid=%s'
+        cursor.execute(query, (log_id, date, unique_id))
+        self.db.commit()
+
+    def connect(self):
+        try:
+            self.connected = False
+            self.db = mysql.connector.connect(
+                host=self.host,
+                user=self.login,
+                password=self.password,
+                database=self.database
+            )
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                logging.log(logging.ERROR, "Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                logging.log(logging.ERROR, "Database does not exist")
+            else:
+                logging.log(logging.ERROR, err)
+        else:
+            self.connected = True
+
+    def close(self):
+        self.db.close()
+        self.connected = False
